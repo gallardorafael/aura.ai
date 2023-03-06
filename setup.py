091a4c91ada771add820149621055a72e9b8b86e
gallardorafael <gagr1050@gmail.com>
2023-03-05 20:31:35 -0600

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+NAME = "aura.ai"
+VERSION = "0.1.0"
+DESCRIPTION = "A Python library to transcribe and summarize voice recordings."
+
+# loading readme
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+# setting up
+setup(
+    name=NAME,
+    version=VERSION,
+    author="Rafael Gallardo",
+    description=DESCRIPTION,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "audiofile",
+        "pyaudio",
+    ],
+    extras_require={
+        "dev": [
+            "pre-commit==2.19.0",
+        ],
+    },
+    keywords=["aura.ai"],
+    classifiers=[
+        "Development Status :: Alpha",
+        "Operating System :: OS Independent",
+        "License :: Undefined",
+    ],
+)
