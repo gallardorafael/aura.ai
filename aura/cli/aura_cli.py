174da56a037c52d99e0805c22ebc5480c47fa588
gallardorafael <gagr1050@gmail.com>
2023-03-18 20:57:21 -0600

    feat: first functional interactive cli

--- aura/cli/aura_cli.py
+++ aura/cli/aura_cli.py
@@ -6,25 +6,24 @@ recorder = AudioRecorder()
 def start_recording():
     print("Starting...")
     print(f"Working with event {recorder.event_uuid}.")
-    recorder.start()
+    recorder.start_recording()
 
 def stop_recording():
     print("Stopping...")
     print(f"Saving records from event {recorder.event_uuid}, in folder: {recorder.root_path}.")
-    recorder.stop()
-    recorder.join()
+    recorder.stop_recording()
 
 def main():
     while True:
         user_input = input("Enter a command (start/stop/q): ")
         if user_input == "start":
-            if not recorder.is_alive():
+            if not recorder.recording:
                 start_recording()
         elif user_input == "stop":
-            if recorder.is_alive():
+            if recorder.recording:
                 stop_recording()
         elif user_input == "q":
-            if recorder.is_alive():
+            if recorder.recording:
                 stop_recording()
             exit()
         else:
