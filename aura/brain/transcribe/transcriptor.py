9b3b8c777c5ebc79db6cf5f2d7d831c190c12094
Rafael <gagr1050@gmail.com>
2023-10-19 22:11:20 -0600

    refactor!: make Transcriptor an abstract class

--- aura/brain/transcribe/transcriptor.py
+++ aura/brain/transcribe/transcriptor.py
@@ -1,10 +1,11 @@
-import whisper
+from abc import ABC, abstractmethod
 
 
-class Transcriptor:
+class Transcriptor(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def transcribe(self):
         """
         Function that receives the path of an audio file, then it transcribes the audio file and postprocess the text, in order to return a list of dictionaries
@@ -17,40 +18,16 @@ class Transcriptor:
             }
         The list will be ordered by the start_time of the transcription.
         """
+        ...
 
-        raise NotImplementedError
-
+    @abstractmethod
     def detect_language(self):
-        raise NotImplementedError
+        ...
 
+    @abstractmethod
+    def _preprocess():
+        ...
+
+    @abstractmethod
     def _postprocess():
-        raise NotImplementedError
-
-
-class WhisperTranscriptor(Transcriptor):
-    def __init__(self, model_path=None):
-        super().__init__()
-        self.model = whisper.load_model(model_path)
-
-    def transcribe(self, audio_path):
-        whisper_output = self.model.transcribe(audio_path)
-        return self._postprocess(whisper_output)
-
-    def detect_language(self, audio_path):
-        raise NotImplementedError
-
-    def load_model(self, model_path):
-        raise NotImplementedError
-
-    def _postprocess(whisper_output: dict):
-        """
-        Function that postprocess the output of the whisper model, in order to return a list of dictionaries
-        with the following structure:
-            {
-                "speaker_id": "speaker_id",
-                "text": "text of the transcription",
-                "start_time": 0.0,
-                "end_time": 1.0
-            }
-        The list will be ordered by the start_time of the transcription.
-        """
+        ...
